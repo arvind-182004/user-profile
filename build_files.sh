4f1aa3508519caf50e16d2a2faa6544bf5da72b1
arvind kumar <ncr.arvindkumar1@gmail.com>
2025-10-06 22:33:59 +0530

    build fixed static file

--- build_files.sh
+++ build_files.sh
@@ -5,6 +5,6 @@ python3.12 -m pip install -r requirements.txt
 echo "📦 Running collectstatic..."
 python3.12 manage.py makemigrations --noinput
 python3.12 manage.py migrate --noinput
-python3.12 manage.py collectstatic --noinput
+# python3.12 manage.py collectstatic --noinput
 
 echo "✅ Build complete!"
