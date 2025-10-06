68fb3c86fe51e52c40ae728a73df9e9030fb1771
arvind kumar <ncr.arvindkumar1@gmail.com>
2025-10-06 22:50:09 +0530

    last try

--- api/urls.py
+++ api/urls.py
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('demo',include('demo.urls')),
-]
+]+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+
